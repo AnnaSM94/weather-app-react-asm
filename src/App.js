0594033f98e0003854ebb5c0597e14d4c4382062
AnnaSM94 <161344697+AnnaSM94@users.noreply.github.com>
2024-05-04 22:21:51 +0100

    Moved footer to App.js and polished CSS

--- src/App.js
+++ src/App.js
@@ -4,7 +4,37 @@ import Weather from "./Weather";
 function App() {
   return (
     <div className="App">
-      <Weather />
+      <div className="weather-app">
+        <Weather />
+        <footer>
+          <p>
+            This project was coded by{" "}
+            <a
+              href="https://github.com/AnnaSM94"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Anna Serra
+            </a>
+            , code is open-sourced on{" "}
+            <a
+              href="https://github.com/AnnaSM94/weather-app-react-asm"
+              target="_blank"
+              rel="noreferrer"
+            >
+              GitHub
+            </a>{" "}
+            and hosted on{" "}
+            <a
+              href="https://weather-app-react-asm.netlify.app/"
+              target="_blank"
+              rel="noreferrer"
+            >
+              Netlify
+            </a>
+          </p>
+        </footer>
+      </div>
     </div>
   );
 }
--- src/Weather.js
+++ src/Weather.js
@@ -18,74 +18,44 @@ export default function Weather() {
 
   return (
     <div className="Weather">
-      <div className="weather-app">
-        <header>
-          <form onSubmit={handleWeather}>
-            <input
-              type="search"
-              placeholder="Enter a city..."
-              required
-              className="search-input"
-              onChange={changeCity}
-            />
-            <input type="submit" value="Search" className="search-button" />
-          </form>
-        </header>
-        <main>
-          <div className="current-weather">
-            <div>
-              <h1 className="current-city">Leeds</h1>
-              <p className="current-details">
-                <span>Friday 21:00,</span> <span>light rain</span>
-                <br />
-                Humidity: <strong>80%</strong>, Wind:
-                <strong> 5 mph</strong>
-              </p>
-            </div>
-            <div className="current-temperature">
-              <div>
-                {" "}
-                <img
-                  className="current-temperature-icon"
-                  src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
-                  alt="temperature icon"
-                />
-              </div>
-              <span className="current-temperature-value">6</span>
-              <span className="current-temperature-unit">°C</span>
-            </div>
+      <header>
+        <form onSubmit={handleWeather}>
+          <input
+            type="search"
+            placeholder="Enter a city..."
+            required
+            className="search-input"
+            onChange={changeCity}
+          />
+          <input type="submit" value="Search" className="search-button" />
+        </form>
+      </header>
+      <main>
+        <div className="current-weather">
+          <div>
+            <h1 className="current-city">Leeds</h1>
+            <p className="current-details">
+              <span>Friday 21:00,</span> <span>light rain</span>
+              <br />
+              Humidity: <strong>80%</strong>, Wind:
+              <strong> 5 mph</strong>
+            </p>
           </div>
-          <div className="weather-forecast"></div>
-        </main>
-        <footer>
-          <p>
-            This project was coded by{" "}
-            <a
-              href="https://github.com/AnnaSM94"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Anna Serra
-            </a>
-            , code is open-sourced on{" "}
-            <a
-              href="https://github.com/AnnaSM94/weather-app-react-asm"
-              target="_blank"
-              rel="noreferrer"
-            >
-              GitHub
-            </a>{" "}
-            and hosted on{" "}
-            <a
-              href="https://weather-app-react-asm.netlify.app/"
-              target="_blank"
-              rel="noreferrer"
-            >
-              Netlify
-            </a>
-          </p>
-        </footer>
-      </div>
+          <div className="current-temperature">
+            <div>
+              {" "}
+              <img
+                className="current-temperature-icon"
+                src="https://ssl.gstatic.com/onebox/weather/64/rain_light.png"
+                alt="temperature icon"
+              />
+            </div>
+            <span className="current-temperature-value">6</span>
+            <span className="current-temperature-unit">°C</span>
+          </div>
+        </div>
+        <div className="weather-forecast"></div>
+      </main>
     </div>
   );
 }
